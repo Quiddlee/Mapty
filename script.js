108c51cedac2dbb1c958c0883b4a39aff19a65de
Bogdan <thegarrybit@gmail.com>
2023-05-05 15:38:15 +0300

    init: add custom marker popup

--- script.js
+++ script.js
@@ -28,8 +28,20 @@ if (navigator.geolocation) {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
 
-      L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', (mapEvent) => {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([ lat, lng ])
+          .addTo(map)
+          .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+          }))
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     }, errorHandler('Could not get your position'));
 } 
